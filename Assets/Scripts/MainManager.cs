4b86e99a062dd45df561b024ef3a5fb5be816d60
AkioMabuchi <mabuchiakio@gmail.com>
2020-02-04 00:17:50 +0900

    big update

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,78 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class MainManager : MonoBehaviour {
 
-    BoardManager boardManager;
+    GameObject gameObjectCanvas;
 
-    [SerializeField] GameObject mainCamera;
+    GameObject gameObjectPanelMain;
+    GameObject gameObjectPanelIntroduction;
+    GameObject gameObjectPanelTitle;
 
-    [SerializeField] GameObject gameObjectPanelTitle;
-    [SerializeField] GameObject gameObjectPanelSelectLevel;
+    GameObject gameObjectButtonStart;
+    GameObject gameObjectButtonLogin;
+    GameObject gameObjectButtonSettings;
 
-    [SerializeField] GameObject gameObjectTitles;
+    Image imageIntroduction;
+    Image imageTitle;
 
-	// Use this for initialization
-	void Start () {
-        if (Screen.height * 9 > Screen.width * 16)
-        {
-            int size = Screen.height * 540 / Screen.width;
-            mainCamera.GetComponent<Camera>().orthographicSize = size;
-        }
+    // Use this for initialization
+    void Start () {
+        gameObjectCanvas = GameObject.Find("Canvas");
 
-        boardManager = GameObject.Find("BoardManager").GetComponent<BoardManager>();
+        gameObjectPanelMain = gameObjectCanvas.transform.Find("PanelMain").gameObject;
+        gameObjectPanelIntroduction = gameObjectPanelMain.transform.Find("PanelIntroduction").gameObject;
+        gameObjectPanelTitle = gameObjectPanelMain.transform.Find("PanelTitle").gameObject;
+
+        imageIntroduction = gameObjectPanelIntroduction.transform.Find("ImageIntroduction").gameObject.GetComponent<Image>();
+        imageTitle = gameObjectPanelTitle.transform.Find("ImageTitle").gameObject.GetComponent<Image>();
+
+        gameObjectButtonStart = gameObjectPanelTitle.transform.Find("ButtonStart").gameObject;
+        gameObjectButtonLogin = gameObjectPanelTitle.transform.Find("ButtonLogin").gameObject;
+        gameObjectButtonSettings = gameObjectPanelTitle.transform.Find("ButtonSettings").gameObject;
+
+        StartCoroutine(CoroutineIntroduction());
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    public void SetActivePanelTitle()
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    public void OnClickButtonStart()
     {
+        StartCoroutine(CoroutineOnClickButtonStart());
+    }
+
+    public void OnClickButtonLogin()
+    {
+        StartCoroutine(CoroutineOnClickButtonLogin());
+    }
+
+    public void OnClickButtonLoginSubmit()
+    {
+        StartCoroutine(CoroutineOnClickButtonLoginSubmit());
+    }
+
+    public void OnClickButtonAtelier144Signup()
+    {
+        StartCoroutine(CoroutineOnClickButtonAtelier144Signup());
+    }
+
+    IEnumerator CoroutineIntroduction()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        // イントロダクションを徐々に表示させて、徐々に消滅させるDOTween Sequense
+        Sequence sequenceImageIntroduction = DOTween.Sequence();
+        sequenceImageIntroduction.Append(imageIntroduction.DOColor(new Color(1.0f, 1.0f, 1.0f, 1.0f), 3.0f));
+        sequenceImageIntroduction.Append(imageIntroduction.DOColor(new Color(1.0f, 1.0f, 1.0f, 0.0f), 3.0f));
+
+        yield return new WaitForSeconds(6.0f);
+        gameObjectPanelIntroduction.SetActive(false);
         gameObjectPanelTitle.SetActive(true);
-    }
 
-    public void MoveToMain()
-    {
-        boardManager.Initialize();
-    }
+        imageTitle.DOColor(new Color(1.0f, 1.0f, 1.0f, 1.0f), 4.0f);
 
-    public void PushLoginButton()
-    {
+        yield return new WaitForSeconds(4.0f);
+        imageTitle.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
-    }
-
-    public void PushOfflineStartButton()
-    {
-        gameObjectPanelTitle.SetActive(false);
-        gameObjectPanelSelectLevel.SetActive(true);
-    }
-
-    public void PushOnlineStartButton()
-    {
+        gameObjectButtonStart.SetActive(true);
+        gameObjectButtonLogin.SetActive(true);
+        gameObjectButtonSettings.SetActive(true);
 
     }
 
-    public void PushOptionButton()
+    IEnumerator CoroutineOnClickButtonStart()
     {
-
+        yield return new WaitForSeconds(1.0f);
+        gameObjectPanelMain.transform.DOLocalMove(new Vector3(810.0f, 0.0f, 0.0f), 0.5f);
     }
 
-    public void PushEasyButton()
+    IEnumerator CoroutineOnClickButtonLogin()
     {
-        gameObjectPanelSelectLevel.SetActive(false);
-        gameObjectTitles.GetComponent<Titles>().SetTrigger("MoveOut");
+        yield return new WaitForSeconds(1.0f);
+        gameObjectPanelMain.transform.DOLocalMove(new Vector3(810.0f, 0.0f, 0.0f), 0.5f);
     }
 
-    public void PushNormalButton()
+    IEnumerator CoroutineOnClickButtonLoginSubmit()
     {
-
+        SceneManager.LoadScene("LoadingScene", LoadSceneMode.Additive);
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.UnloadSceneAsync("LoadingScene");
     }
 
-    public void PushHardButton()
+    IEnumerator CoroutineOnClickButtonAtelier144Signup()
     {
-
+        yield return new WaitForSeconds(1.0f);
+        Application.OpenURL("https://gameatelier144.com/signup");
     }
 }
